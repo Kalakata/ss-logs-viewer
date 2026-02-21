5315092f092bbe23ab04a4316787de1e99ce32b6
Kaloyan Donchev <kal.donchev@gmail.com>
2026-02-21 17:04:05 +0200

    Fix Warehouse column to use vendor_name instead of description

--- logs/management/commands/send_daily_report.py
+++ logs/management/commands/send_daily_report.py
@@ -83,7 +83,7 @@ class Command(BaseCommand):
                 l.get('units', 0),
                 qty_change,
                 l.get('product_name', ''),
-                l.get('description', ''),
+                l.get('vendor_name', ''),
                 l.get('user_name', ''),
                 l.get('mismatch', ''),
             ])
