c7b8b8e93aa085fba611048165b30e481b2f9d80
Kaloyan Donchev <kal.donchev@gmail.com>
2026-04-07 12:35:01 +0300

    Remove old pages, single explorer route at /

--- logs/urls.py
+++ logs/urls.py
@@ -2,9 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('api/search-groups/', views.search_groups, name='search_groups'),
-    path('group/<str:group_id>/logs/', views.group_logs, name='group_logs'),
-    path('movements/', views.movements, name='movements'),
+    path('', views.explorer, name='explorer'),
     path('cron/send-report/', views.trigger_report, name='trigger_report'),
 ]
